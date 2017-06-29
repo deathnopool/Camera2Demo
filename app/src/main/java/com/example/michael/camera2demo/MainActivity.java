e790f8f4f399bd73084fc39a51d27d2cc68e5e49
deathnopool <deathnopool@gmail.com>
2017-06-29 14:18:13 +0800

    delete unused package

--- app/src/main/java/com/example/michael/camera2demo/MainActivity.java
+++ app/src/main/java/com/example/michael/camera2demo/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
-import android.graphics.drawable.GradientDrawable;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
